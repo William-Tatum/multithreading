1cb758c429f9c8fcf9927e7acbc96a3c12b93b60
William Tatum <willtaaa@live.com>
2023-11-12 03:16:25 -0600

    updates

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # multithreading
-my experience following the little book of semaphores
+My experience following the little book of semaphores.
+
+**sync**
+This is a file written in both Python and C++ showing the rendezvous problem.
+
+If A arrives first, it waits for B. When B arrives, it wakes A and might
+proceed immediately to its wait in which case it blocks, allowing A to reach its
+signal, after which both threads can proceed.
---
+++ sync.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <semaphore.h>
+#include <thread>
+
+sem_t aArrived;
+sem_t bArrived;
+
+void A() {
+    printf("Drone A: Picked up the package.\n");
+    sem_post(&bArrived);
+    sem_wait(&aArrived);
+    printf("Drone A: Package handed off to Drone B.\n");
+}
+
+void B() {
+    sem_wait(&bArrived);
+    printf("Drone B: Arrived at the redezvous point.\n");
+    sem_post(&aArrived);
+    printf("Drone B: Received package and is delivering to the final destination.\n");
+}
+
+int main() {
+    sem_init(&aArrived, 0, 0);
+    sem_init(&bArrived, 0, 0);
+
+    std::thread t1(A);
+    std::thread t2(B);
+    
+    t1.join();
+    t2.join();
+
+    sem_destroy(&aArrived);
+    sem_destroy(&bArrived);
+}
